4ef6b1b712c226b9ff5793d1d08cb88c62e5d387
Imadh Ajaz <93546632+imadh-a@users.noreply.github.com>
2024-09-30 18:10:16 -0500

    Update quicksort_random_and_non-random.py

--- Hands-On 6/quicksort_random_and_non-random.py
+++ Hands-On 6/quicksort_random_and_non-random.py
@@ -9,9 +9,9 @@ def partition(arr, low, high):
             arr[i], arr[j] = arr[j], arr[i]
     arr[i + 1], arr[high] = arr[high], arr[i + 1]
     return i + 1
-
+#quicksort main
 def quicksort(arr, low, high):
-    if low < high:
+    if low < high: 
         pi = partition(arr, low, high)
         quicksort(arr, low, pi - 1)
         quicksort(arr, pi + 1, high)
